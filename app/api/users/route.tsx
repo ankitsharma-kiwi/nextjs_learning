e99afc7d9ea3d948af862391ff13cc5068e09026
ankitsharma-kiwi <ankit.sharma@kiwitech.com>
2024-09-18 17:15:37 +0530

    post a user

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -6,3 +6,9 @@ export function GET(request: NextRequest) {
         { id: 2, name: 'Jane Smith' },
     ]);
 }
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    if(!body.name) return NextResponse.json({error: 'Name is required'}, {status: 400});
+    return NextResponse.json(body, {status: 201});
+}

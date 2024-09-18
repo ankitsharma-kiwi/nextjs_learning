98c92db8fdf3febc00f7b715c5c1e824ce8cc222
ankitsharma-kiwi <ankit.sharma@kiwitech.com>
2024-09-18 13:57:02 +0530

    sort name by fast-sort

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,28 +1,28 @@
-'use client'
 import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { sort } from 'fast-sort'
+interface User {
+  id: number
+  name: string
+  email: string
+}
 
-const UserTable = ({ data, searchParams }: any) => {
-    const [sortData, setSortData] = useState<any[]>([]);
+const UserTable = async ({ searchParams }: any) => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store' })
+  const data: User[] = await res.json()
 
-    useEffect(() => {
-        if (searchParams.sortOrder === 'name') {
-            const sortedData = [...data].sort((a, b) => a.name.localeCompare(b.name));
-            setSortData(sortedData);
-        } else if (searchParams.sortOrder === 'email') {
-            const sortedData = [...data].sort((a, b) => a.email.localeCompare(b.email));
-            setSortData(sortedData);
-        } else {
-            setSortData(data);
-        }
-    }, [searchParams, data]);
+  const sortData = sort(data).asc(searchParams.sortOrder === 'email' ? (u) => u.email : (u) => u.name)
 
-    return (
+  return (
     <table className="table table-bordered ">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Email</th>
+          <th>
+            <Link href={'/users?sortOrder=name'}>Name</Link>
+          </th>
+          <th>
+            <Link href={'/users?sortOrder=email'}>Email</Link>
+          </th>
         </tr>
       </thead>
       <tbody>
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,25 +3,16 @@ import AddToCart from '../Components/AddToCart'
 import Link from 'next/link'
 import UserTable from './UserTable'
 
-interface User {
-  id: number
-  name: string
-  email: string
-}
-
 interface UserPageProps {
   searchParams: { [key: string]: string | string[] | undefined }
 }
 
 const page = async ({ searchParams }: UserPageProps) => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store' })
-  const data: User[] = await res.json()
-
   return (
     <>
       <AddToCart />
       <h1>Users Table</h1>
-      <UserTable data={data} searchParams={searchParams} />
+      <UserTable searchParams={searchParams} />
     </>
   )
 }

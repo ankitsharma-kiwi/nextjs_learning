451a30c99115722c6e1d8ebfe7b9b8a638f3745c
ankitsharma-kiwi <ankit.sharma@kiwitech.com>
2024-09-18 17:00:35 +0530

    api routes, for all and single user.

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function GET(request: NextRequest, { params }: { params: { id: number } }) {
+  if (params.id > 10) return NextResponse.json({ message: 'User not found' }, { status: 404 })
+
+  return NextResponse.json({ id: params.id, name: 'John Doe' })
+}
